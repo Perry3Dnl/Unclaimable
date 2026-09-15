e4508076c8b0eacde37191634f4880f6cddb1b17
Perry <its_Q@hotmail.com>
2026-09-15 15:48:22 +0200

    Add curated celebrity name data for v0.7.3

---
+++ platforms/dotnet/src/Unclaimable/Checker.Celebrities.cs
@@ -0,0 +1,25 @@
+namespace Unclaimable;
+
+internal static class CelebrityData
+{
+    internal const string ReservationPrefix = "__unclaimable_internal_celebrity_rule__:";
+
+    // Curated v0.7.3 starter set. These are protected identifier forms, not substring roots.
+    // Matching may additionally use the configured compact, obfuscation, and Unicode-confusable pipelines.
+    internal static readonly string[] Names =
+    {
+        "trump", "donaldtrump", "obama", "barackobama", "elonmusk", "billgates", "jeffbezos", "markzuckerberg",
+        "oprah", "kimkardashian", "kyliejenner", "cristianoronaldo", "ronaldo", "lionelmessi", "messi", "lebronjames",
+        "michaeljordan", "serenawilliams", "rogerfederer", "rafaelnadal", "novakdjokovic", "lewishamilton", "maxverstappen",
+        "davidbeckham", "neymar", "kylianmbappe", "tigerwoods", "simonebiles", "usainbolt", "miketyson", "stephencurry",
+        "viratkohli", "shahrukhkhan", "taylorswift", "beyonce", "rihanna", "drake", "eminem", "adele", "edsheeran",
+        "brunomars", "justinbieber", "selenagomez", "arianagrande", "ladygaga", "madonna", "shakira", "theweeknd",
+        "badbunny", "dualipa", "kendricklamar", "snoopdogg", "billieeilish", "katyperry", "mileycyrus", "nickiminaj",
+        "cardib", "postmalone", "harrystyles", "jenniferlopez", "britneyspears", "mariahcarey", "celinedion", "travisscott",
+        "jayz", "drdre", "tomcruise", "leonardodicaprio", "bradpitt", "angelinajolie", "dwaynejohnson", "willsmith",
+        "keanureeves", "robertdowneyjr", "chrishemsworth", "scarlettjohansson", "zendaya", "timotheechalamet", "margotrobbie",
+        "ryanreynolds", "hughjackman", "johnnydepp", "morganfreeman", "samuelljackson", "emmawatson", "danielradcliffe",
+        "jenniferaniston", "georgeclooney", "nicolekidman", "natalieportman", "annehathaway", "sandrabullock", "juliaroberts",
+        "tomhanks", "stevenspielberg", "georgelucas", "jamescameron", "mrbeast", "priyankachopra", "jackiechan"
+    };
+}
